d42b372a9f1c73a8edc81aeafd1f4dc89763eb34
niked <meetniked@gmail.com>
2020-09-28 13:46:38 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, {Component} from 'react'
+import Navigation from './components/navigation';
+import Header from './components/header';
+import Features from './components/features';
+import About from './components/about';
+import Services from './components/services';
+import Gallery from './components/gallery';
+import Testimonials from './components/testimonials';
+import Team from './components/Team';
+import Contact from './components/contact';
+import JsonData from './data/data.json';
+
+export class App extends Component {
+    state = {
+        landingPageData: {},
+    }
+
+    getlandingPageData() {
+        this.setState({landingPageData: JsonData})
+    }
+
+    componentDidMount() {
+        this.getlandingPageData();
+    }
+
+    render() {
+        const {landingPageData} = this.state;
+        return (
+            <div>
+                <Navigation/>
+                <Header data={landingPageData.Header}/>
+                <Features data={landingPageData.Features}/>
+                <About data={landingPageData.About}/>
+                <Services data={landingPageData.Services}/>
+                <Gallery/>
+                <Testimonials data={landingPageData.Testimonials}/>
+                <Team data={landingPageData.Team}/>
+                <Contact data={landingPageData.Contact}/>
+            </div>
+        )
+    }
+}
+
+export default App;
